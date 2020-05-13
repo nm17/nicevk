96609905ba4b9cb4818a45b1eaa970160d8b8e62
nm17 <dannevergame@gmail.com>
2020-05-13 13:34:45 +0400

    feat: add captcha support (wip)
    Solves captcha once and then quits.

--- nicevk/api.py
+++ nicevk/api.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
-from vkbottle import User, Message
+from loguru import logger
+from python_rucaptcha.ImageCaptcha import ImageCaptcha, aioImageCaptcha
+from vkbottle import User, Message, VKError
 import dotenv
 from vkbottle.framework import Middleware
 import json
+
+from nicevk.errors import CaptchaError
+
 
 nicevk_folder = Path.home().joinpath("nicevk").absolute()
 
@@ -17,10 +22,25 @@ commands = [".help"]
 @user.middleware.middleware_handler()
 class NoBotMiddleware(Middleware):
     async def middleware(self, message: Message):
-        if message.from_id == (await message.api.users.get())[0].id:
-            return True
+        return message.from_id == (await message.api.users.get())[0].id
+
+
+rucaptcha = aioImageCaptcha(rucaptcha_key=env.get("RUCAPTCHA_TOKEN", ""))
+
+
+@logger.catch
+async def solve_captcha(e: VKError):
+    captcha_img, captcha_sid = e.raw_error["captcha_img"], e.raw_error["captcha_sid"]
+    while True:
+        resp = await rucaptcha.captcha_handler(captcha_link=captcha_img)
+        if not resp["error"]:
+            print(e.method_requested)
+            print(e.params_requested)
+            print(e.raw_error)
+            await user.api.api(method="messages.edit", params={**e.params_requested, "captcha_key": resp['captchaSolve'], "captcha_sid": captcha_sid})
+            return
         else:
-            return False
+            raise CaptchaError(resp['errorBody']['text'])
 
 
 state_file = nicevk_folder / "state.json"
